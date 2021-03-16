ff3847dc1cc7838319cdc740262776e66b9c1529
KXRXH <74421236+KXRXH@users.noreply.github.com>
2021-03-16 21:45:59 +0700

    Add files via upload

--- CarInfo.py
+++ CarInfo.py
@@ -1,97 +1,81 @@
-import json
+import configparser
 import os
+import platform
+import sys
 
 import ac
 
-import acsys
+from Data.CarPerfomanceTest import avgSpeedTest
+from Data.CarStats import carStatsOutput, getCarStats
 
-carName = ac.getCarName(0)
-carPATH = os.path.realpath("content/cars/" + carName + '/ui/ui_car.json').replace("\\", "/")
-ac.log(carPATH)
-speed = 0
-gear = 0
-rpm = 0
-avg_speed = 0
-curr_max_Speed = 0
-to100 = 0
-sr = 0
-sr_Speed = 0
-boost = 0
-with open(carPATH, "r", encoding='utf-8-sig') as read_file:
-    data = json.load(read_file)
-    carMaxRPM = float(data["torqueCurve"][-1][0])
+if platform.architecture()[0] == "64bit":
+    libdir = "stdlib/lib64"
+else:
+    libdir = "stdlib/lib"
 
-
-    def acMain(ac_version):
-        global speed, gear, rpm, avg_speed, boost
-
-        appWindow = ac.newApp("CarInfo")
-        ac.setSize(appWindow, 150, 240)
-        speed = ac.addLabel(appWindow, "KMH:P ")
-        avg_speed = ac.addLabel(appWindow, "AVG KMH: ")
-        gear = ac.addLabel(appWindow, "Gear: ")
-        rpm = ac.addLabel(appWindow, "RPM: ")
-        boost = ac.addLabel(appWindow, "Boost: ")
-
-        ac.setPosition(speed, 3, 30)
-        ac.setPosition(rpm, 3, 70)
-        ac.setPosition(gear, 3, 110)
-        ac.setPosition(avg_speed, 3, 190)
-        ac.setPosition(boost, 3, 150)
-
-        return "CarInfo"
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), libdir))
+os.environ['PATH'] = os.environ['PATH'] + ";."
+from stdlib.sim_info import info
+PATH = os.path.realpath(os.getcwd() + '/apps/python/CarInfo/config.ini')
+config = configparser.ConfigParser()
+config.read(PATH)
+ac.log(os.getcwd())
+scaleSpeed = 1 if config["Scales"]["Speed"][0] == "0" else 0.609
+scaleTurbo = 1 if config["Scales"]["TurboPressure"][0] == "0" else 14.504
+count = 0
+Avg_Speed = 0
+speed = None
+gear = None
+rpm = None
+avg_speed = None
+boost = None
+resetBtn = None
 
 
-    def acUpdate(deltaT):
-        global speed, gear, rpm, avg_speed, curr_max_Speed, sr, sr_Speed, boost, carName, carMaxRPM
+def acMain(ac_version):
+    global speed, gear, rpm, avg_speed, boost, resetBtn
+    MainWindow = ac.newApp("CarInfo")
+    PerfomanceWindow = ac.newApp("Perfomance")
+    ac.setBackgroundOpacity(MainWindow, 0.3)
+    ac.setSize(MainWindow, 150, 150)
+    ac.setSize(PerfomanceWindow, 150, 150)
+    speed = ac.addLabel(MainWindow, "KMH: None ")
+    avg_speed = ac.addLabel(PerfomanceWindow, "AVG KMH: None")
+    gear = ac.addLabel(MainWindow, "Gear: None")
+    rpm = ac.addLabel(MainWindow, "RPM: None")
+    boost = ac.addLabel(MainWindow, "Boost: None")
+    resetBtn = ac.addButton(PerfomanceWindow, "Reset")
+    ac.setSize(resetBtn, 30, 20)
+    ac.addOnClickedListener(resetBtn, resetAVG)
 
-        curr_RPM = ac.getCarState(0, acsys.CS.RPM)
+    ac.setIconPosition(MainWindow, 0, -10000)
+    ac.setIconPosition(PerfomanceWindow, 0, -10000)
 
-        curr_Speed = ac.getCarState(0, acsys.CS.SpeedKMH)
+    ac.setPosition(speed, 3, 30)
+    ac.setPosition(rpm, 3, 60)
+    ac.setPosition(gear, 3, 90)
+    ac.setPosition(avg_speed, 3, 30)
+    ac.setPosition(boost, 3, 120)
 
-        curr_Gear = ac.getCarState(0, acsys.CS.Gear)
+    return "CarInfo"
 
-        curr_boost = ac.getCarState(0, acsys.CS.TurboBoost)
 
-        sr_Speed += round(curr_Speed, 1)
-        sr += 1
+def resetAVG(*args):
+    global Avg_Speed, count
+    Avg_Speed = 0
+    count = 0
 
-        if curr_max_Speed < curr_Speed:
-            curr_max_Speed = curr_Speed
 
-        if round(curr_Speed) == 0:
-            curr_max_Speed = 0
-            sr_Speed = 0
-            sr = 0
-            ac.setVisible(avg_speed, 0)
-        else:
-            ac.setVisible(avg_speed, 1)
-        if curr_boost > 2:
-            ac.setFontColor(boost, 1, 0, 0, 1)
-        elif 1 < curr_boost < 2:
-            ac.setFontColor(boost, 1, 0.9, 0, 1)
-        elif curr_boost < 1:
-            ac.setFontColor(boost, 1, 1, 1, 1)
+def acUpdate(deltaT):
+    global speed, gear, rpm, avg_speed, count, Avg_Speed, boost, scaleSpeed, scaleTurbo
+    if round(getCarStats()[0], 1) >= 1:
+        Avg_Speed += round(getCarStats()[0], 1)
+        count += 1
 
-        if carMaxRPM - 1250 < curr_RPM:
-            ac.setFontColor(rpm, 1, 0, 0, 1)
-            if curr_Gear != 1:
-                ac.setFontColor(gear, 1, 0, 0, 1)
+    if round(getCarStats()[0]) == 0:
+        Avg_Speed = 0
+        count = 0
+        ac.setText(avg_speed, "AVG KMH: Waiting...")
 
-        elif carMaxRPM - 1250 > curr_RPM > carMaxRPM - 2500:
-            ac.setFontColor(rpm, 1, 0.9, 0, 1)
-            ac.setFontColor(gear, 1, 1, 1, 1)
-        else:
-            ac.setFontColor(rpm, 1, 1, 1, 1)
-            ac.setFontColor(gear, 1, 1, 1, 1)
-
-        ac.setText(speed, "KMH {}".format(round(curr_Speed, 1)))
-        if curr_Gear == 0:
-            ac.setText(gear, "Gear {}".format("R"))
-        elif curr_Gear == 1:
-            ac.setText(gear, "Gear {}".format("N"))
-        else:
-            ac.setText(gear, "Gear {}".format(curr_Gear - 1))
-        ac.setText(boost, "Boost: {}".format(round(curr_boost, 2)))
-        ac.setText(rpm, "RPM {}".format(round(curr_RPM)))
-        ac.setText(avg_speed, "AVG KMH {}".format(round(sr_Speed / sr, 1)))
+    avgSpeedTest(count, Avg_Speed, avg_speed, scaleSpeed)
+    carStatsOutput(boost, gear, rpm, speed, info.static.maxRpm, scaleTurbo, scaleSpeed)
---
+++ Data/CarStats.py
@@ -0,0 +1,55 @@
+import ac
+import acsys
+
+
+def getCarStats():
+    curr_RPM = ac.getCarState(0, acsys.CS.RPM)
+    curr_Speed = ac.getCarState(0, acsys.CS.SpeedKMH)
+    curr_Gear = ac.getCarState(0, acsys.CS.Gear)
+    curr_boost = ac.getCarState(0, acsys.CS.TurboBoost)
+
+    return [curr_Speed, curr_RPM, curr_Gear, curr_boost]
+
+
+def carStatsOutput(boost_label, gear_label, rpm_label, speed_label, maxRPM, scaleTurbo=1, scaleSpeed=1):
+    curr_RPM = ac.getCarState(0, acsys.CS.RPM)
+
+    curr_Speed = ac.getCarState(0, acsys.CS.SpeedKMH) * scaleSpeed
+
+    curr_Gear = ac.getCarState(0, acsys.CS.Gear)
+
+    curr_boost = ac.getCarState(0, acsys.CS.TurboBoost)
+
+    if curr_boost > 2:
+        ac.setFontColor(boost_label, 1, 0, 0, 1)
+    elif 1 < curr_boost < 2:
+        ac.setFontColor(boost_label, 1, 0.9, 0, 1)
+    elif curr_boost < 1:
+        ac.setFontColor(boost_label, 1, 1, 1, 1)
+
+    if maxRPM - 500 < curr_RPM:
+        ac.setFontColor(rpm_label, 1, 0, 0, 1)
+        if curr_Gear != 1:
+            ac.setFontColor(gear_label, 1, 0, 0, 1)
+
+    elif maxRPM - 500 > curr_RPM > maxRPM - 1250:
+        ac.setFontColor(rpm_label, 1, 0.9, 0, 1)
+        ac.setFontColor(gear_label, 1, 1, 1, 1)
+    else:
+        ac.setFontColor(rpm_label, 1, 1, 1, 1)
+        ac.setFontColor(gear_label, 1, 1, 1, 1)
+    if scaleTurbo == 1:
+        ac.setText(boost_label, "Boost: {} bar".format(round(curr_boost * scaleTurbo, 2)))
+    else:
+        ac.setText(boost_label, "Boost: {} psi".format(round(curr_boost * scaleTurbo, 2)))
+    ac.setText(rpm_label, "RPM: {}".format(round(curr_RPM)))
+    if scaleSpeed == 1:
+        ac.setText(speed_label, "KM/H: {}".format(round(curr_Speed, 1)))
+    else:
+        ac.setText(speed_label, "MP/H: {}".format(round(curr_Speed, 1)))
+    if curr_Gear == 0:
+        ac.setText(gear_label, "Gear: {}".format("R"))
+    elif curr_Gear == 1:
+        ac.setText(gear_label, "Gear: {}".format("N"))
+    else:
+        ac.setText(gear_label, "Gear: {}".format(curr_Gear - 1))
